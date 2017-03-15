04c5e5fc98e32c4dbc421165c6d1b9c465b639a3
Alumno <alumno@admins-macbook-pro.local>
2017-03-14 21:10:00 -0500

    crear elementos desde código

---
+++ UiKit dinamico en IOS/NuevoViewController.swift
@@ -0,0 +1,88 @@
+//
+//  NuevoViewController.swift
+//  UiKit dinamico en IOS
+//
+//  Created by Alumno on 14/03/17.
+//  Copyright © 2017 JorgeLimo. All rights reserved.
+//
+
+import UIKit
+
+class NuevoViewController: UIViewController {
+    
+    var vwCabecera:UIView!
+    var lbTitulo:UILabel!
+    var segControler:UISegmentedControl!
+    var btnAceptar:UIButton!
+    var btnCancelar:UIButton!
+    var colorApp:UIColor!
+    //var margen:Float
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let margen:CGFloat = 10
+        
+        colorApp = UIColor(red: 102/255.0, green: 204/255.0, blue: 1, alpha: 1.0)
+        //margen = CGFloat(10)
+
+        // Do any additional setup after loading the view.
+        
+        
+        vwCabecera=UIView(frame: CGRect(x: 0, y: 0, width: self.view.frame.width, height: 60))
+        vwCabecera.backgroundColor = colorApp
+        self.view.addSubview(vwCabecera)
+        
+        //lbTitulo=UILabel(frame: CGRect(x: vwCabecera.frame.origin.x, y: vwCabecera.frame.origin.y, width: vwCabecera.frame.size.width, height: vwCabecera.frame.size.height))
+        
+        lbTitulo = UILabel(frame: vwCabecera.frame)
+        lbTitulo.text="UIKIT Dinamico en IOS"
+        lbTitulo.textColor = UIColor.white
+        lbTitulo.font = UIFont(name: "Helvetica", size: 17)
+        lbTitulo.textAlignment = NSTextAlignment.center
+        vwCabecera.addSubview(lbTitulo)
+        
+        segControler = UISegmentedControl(frame: CGRect(x: margen, y: vwCabecera.frame.size.height + margen, width: self.view.frame.size.width - 20, height: 30))
+        segControler.insertSegment(withTitle: "Primero", at: 0, animated: true)
+        segControler.insertSegment(withTitle: "Segundo", at: 1, animated: true)
+        segControler.tintColor = colorApp
+        segControler.selectedSegmentIndex = 0
+        self.view.addSubview(segControler)
+        
+        btnAceptar = UIButton(frame: CGRect(x: margen, y: self.view.frame.size.height - 30 - margen, width: 120, height: 30))
+        btnAceptar.setTitle("Aceptar", for: UIControlState.normal)
+        btnAceptar.backgroundColor = colorApp
+        //agregamos una accion al boton con un selector y creando la funcion que ejecutara el evento
+        btnAceptar.addTarget(self, action: #selector(aceptar), for: UIControlEvents.touchUpInside)
+        self.view.addSubview(btnAceptar)
+        
+        btnCancelar = UIButton(frame: CGRect(x: self.view.frame.size.width - 120 - margen, y: btnAceptar.frame.origin.y , width: 120, height: 30))
+        btnCancelar.setTitle("Cancelar", for: UIControlState.normal)
+        btnCancelar.setTitleColor(colorApp, for: UIControlState.normal)
+        btnCancelar.addTarget(self, action: #selector(cancelar), for: UIControlEvents.touchDown)
+        self.view.addSubview(btnCancelar)
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func aceptar(sender:UIButton){
+        print("Aceptar")
+    }
+    
+    func cancelar(sender:UIButton){
+        print("Cancelar")
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
